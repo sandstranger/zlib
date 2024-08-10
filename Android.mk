2f63140fe01f76760b676107eb48974e0fc7ff76
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-08-10 15:21:19 +0400

    enable flto

--- Android.mk
+++ Android.mk
@@ -22,7 +22,8 @@ include $(CLEAR_VARS)
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE := libz
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
+LOCAL_CFLAGS += -O3 -flto=thin -DUSE_MMAP
+LOCAL_LDFLAGS += -flto=thin -Wl,-plugin-opt=-emulated-tls -fuse-ld=lld
 LOCAL_SRC_FILES := $(zlib_files)
 ifeq ($(TARGET_ARCH),arm)
   LOCAL_SDK_VERSION := 9
